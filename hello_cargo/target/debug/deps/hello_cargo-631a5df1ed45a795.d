f4d837f2b2f0ddbdb52ee586adeecc7c78687146
samir-s-hassan <hsamir347@gmail.com>
2024-02-20 01:12:08 -0500

    learned cargo run

---
+++ hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795.d
@@ -0,0 +1,5 @@
+/Users/samirhassan/Desktop/CSE262/projects/hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795: src/main.rs
+
+/Users/samirhassan/Desktop/CSE262/projects/hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/samirhassan/Desktop/CSE262/projects/hello_cargo/target/debug/hello_cargo: /Users/samirhassan/Desktop/CSE262/projects/hello_cargo/src/main.rs
